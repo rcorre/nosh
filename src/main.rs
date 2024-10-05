189390a21ee8cd21789e558cea1884c7eef9ea40
Ryan Roden-Corrent <git@rrc.codes>
2024-10-05 17:01:28 -0400

    Add log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::{
+    fs,
+    io::{BufRead, BufReader},
+    path::Path,
+};
+
+fn parse(path: &Path) -> Result<()> {
+    let file = fs::File::open(path)?;
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        log::trace!("Read {line:?}");
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    parse(Path::new("./food.nom"))
 }
